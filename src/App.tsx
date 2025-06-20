a40cc94decaea358e66e991b5624feec332bcbc1
exceed96 <exceed96@naver.com>
2025-06-20 16:25:02 +0900

    pwa 앱 업데이트 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import "./App.css";
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router";
 import { Analytics } from "@vercel/analytics/react";
 
 function App() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/dist/sw.js").then((registration) => {
+          registration.onupdatefound = () => {
+            const newWorker = registration.installing;
+            if (!newWorker) return;
+            newWorker.onstatechange = () => {
+              if (
+                newWorker.state === "installed" &&
+                navigator.serviceWorker.controller
+              ) {
+                if (confirm("새 버전이 있습니다. 새로고침 하시겠습니까?")) {
+                  window.location.reload();
+                }
+              }
+            };
+          };
+        });
+      });
+    }
+  }, []);
+
   return (
     <div className="flex justify-center min-h-svh">
       <RouterProvider router={Router} />
